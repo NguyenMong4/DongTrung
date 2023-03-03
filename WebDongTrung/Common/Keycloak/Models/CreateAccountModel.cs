c8961907e9e4a963048dea78f0bc296f25959525
NguyenMong4 <ndcpham@gmail.com>
2023-03-03 17:38:36 +0700

    update login controller

---
+++ WebDongTrung/Common/Keycloak/Models/CreateAccountModel.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebDongTrung.Common.Keycloak.Models
+{
+    public class CreateAccountModel
+    {
+        [Required(ErrorMessage = "User Name is required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+         [Required(ErrorMessage = "Group is required")]
+        public List<string> groups { get; set; }
+    }
+}
--- WebDongTrung/Controllers/LoginController.cs
+++ WebDongTrung/Controllers/LoginController.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,8 @@ using Microsoft.AspNetCore.Mvc;
 using WebDongTrung.Models;
 using WebDongTrung.Common.Keycloak;
 using System.Text;
+using NuGet.Protocol;
+using WebDongTrung.Common.Keycloak.Models;
 
 namespace WebDongTrung.Controllers
 {
@@ -31,11 +34,12 @@ namespace WebDongTrung.Controllers
                 new KeyValuePair<string, string>("grant_type", "password")
             });
             var respon = await clien.PostAsync($"{url}/realms/{_config["KeyCloak:realms"]}/protocol/openid-connect/token", content);
-            return new JsonResult(JsonSerializer.Deserialize<object>(await respon.Content.ReadAsStringAsync()));
+            var token = new JsonResult(JsonSerializer.Deserialize<object>(await respon.Content.ReadAsStringAsync()));
+            return token;
         }
 
         [HttpPost("create")]
-        public async Task<IActionResult> CreateAccount([FromBody] LoginModel loginModel)
+        public async Task<IActionResult> CreateAccount([FromBody] CreateAccountModel loginModel)
         {
             var token = new KeycloakServiceAccount(url,_config["KeyCloak:realms"],_config["KeyCloak:client_id"],_config["KeyCloak:client_secret"]).GetToken().Result;
             HttpClient client = new();
@@ -46,21 +50,22 @@ namespace WebDongTrung.Controllers
             HttpRequestMessage requestMessage = new()
             {
                 Method = HttpMethod.Post,
-                RequestUri = new Uri("http://localhost:8080/admin/realms/master/users")
+                RequestUri = new Uri($"{url}/admin/realms/{_config["KeyCloak:realms"]}/users")
             };
             requestMessage.Content = new StringContent(JsonSerializer.Serialize(new
             {
                 username = loginModel.Username,
+                groups = loginModel.groups,
                 credentials = new List<object>{
                     new {
                         type = "password",
-                        value = loginModel.Password
+                        value = loginModel.Password,
                     }
                 }
             }), Encoding.UTF8, "application/json");
 
             var respone = await client.SendAsync(requestMessage);
-            var responseMsg = await respone.Content.ReadAsStringAsync();
+            await respone.Content.ReadAsStringAsync();
             if (respone.IsSuccessStatusCode)
             {
                 return Ok();
