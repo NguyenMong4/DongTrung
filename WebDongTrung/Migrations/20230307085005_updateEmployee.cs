4bd1aaa0d1129bbf63e6745c1a436a45d3329418
NguyenMong4 <ndcpham@gmail.com>
2023-03-09 16:35:05 +0700

    add migration

---
+++ WebDongTrung/Migrations/20230307085005_updateEmployee.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebDongTrung.Migrations
+{
+    public partial class updateEmployee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Emp_kbn",
+                table: "Employees");
+
+            migrationBuilder.RenameColumn(
+                name: "PassWord",
+                table: "Employees",
+                newName: "UserName");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Employees",
+                newName: "Group");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Users",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)",
+                oldMaxLength: 255)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserName",
+                table: "Employees",
+                newName: "PassWord");
+
+            migrationBuilder.RenameColumn(
+                name: "Group",
+                table: "Employees",
+                newName: "Name");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Address",
+                keyValue: null,
+                column: "Address",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "Users",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)",
+                oldMaxLength: 255,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Users",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Emp_kbn",
+                table: "Employees",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
